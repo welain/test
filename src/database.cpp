6e4590f68fd72d5af9dd1725b912045f1a76b1a5
welain <a143262801@outlook.com>
2020-09-11 10:03:03 +0800

    master

---
+++ src/database.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Workers
+{
+    int id;
+    string name;
+    string job;
+    Workers(int id, string name, string job)
+    {
+        this->id = id;
+        this->name = name;
+        this->job = job;
+    }
+};
+
+
+
+int main()
+{
+}
